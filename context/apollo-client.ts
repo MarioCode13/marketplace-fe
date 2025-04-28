c95c79d1ede18bfcc4f56cf43dd606a4359b90bd
Mario Liebenberg <mario.liebenberg@hotmail.com>
2025-04-28 15:44:45 +0200

    downgrade react for shadcn

--- context/apollo-client.ts
+++ context/apollo-client.ts
@@ -2,8 +2,9 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:8080/graphql",
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
 })
+
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem("token")
