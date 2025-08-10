b275da65eb8b99528bf1343c44f918e1f0ea6aa8
MarioCode13 <mario.liebenberg@hotmail.com>
2025-08-10 17:48:32 +0200

    page props fix

--- app/(root)/listings/page.tsx
+++ app/(root)/listings/page.tsx
@@ -2,14 +2,13 @@ import { getServerApolloClient } from '@/lib/apollo/server'
 import { GET_LISTINGS } from '@/lib/graphql/queries'
 import Listings from './Listings'
 
-interface PageProps {
-  params: { [key: string]: string | undefined }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export default async function Page({ searchParams }: PageProps) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Record<string, string | string[] | undefined>
+}) {
   const client = getServerApolloClient()
-  const pageParam = searchParams.page
+  const pageParam = searchParams?.page
   const page = Number(Array.isArray(pageParam) ? pageParam[0] : pageParam) || 1
   // Fetch more items for client-side filtering
   const limit = 100 // Increased limit for client-side filtering
